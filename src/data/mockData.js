2affabbcd193c1e1f95d5304b366f6e6ff5241d0
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-10-30 21:33:11 -0500

    Añadir datos hardcodeados

---
+++ src/data/mockData.js
@@ -0,0 +1,8 @@
+export const categorias = [
+  { id: 1, nombre: "Matemáticas", icono: "📐", color: "bg-blue-500" },
+  { id: 2, nombre: "Ciencias", icono: "🔬", color: "bg-green-500" },
+  { id: 3, nombre: "Programación", icono: "💻", color: "bg-purple-500" },
+  { id: 4, nombre: "Idiomas", icono: "🌍", color: "bg-yellow-500" },
+  { id: 5, nombre: "Historia", icono: "📚", color: "bg-red-500" },
+  { id: 6, nombre: "Arte", icono: "🎨", color: "bg-pink-500" },
+];
